0dfbe2bb13c20a5ac3fe23e72457f2423e44206a
Nischal-S-Dwaral <nischal.dwaral@gmail.com>
2023-06-24 14:35:45 +0100

    Testing GitLab connectivity

--- src/main/java/uos/msc/project/documentation/coverage/comments/scanner/CommentsScannerApplication.java
+++ src/main/java/uos/msc/project/documentation/coverage/comments/scanner/CommentsScannerApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CommentsScannerApplication {
 
+	/**
+	 * Main method to run the spring boot application
+	 * @param args arguments to pass to
+	 * the SpringBootApplication on startup.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CommentsScannerApplication.class, args);
 	}
